7fd3f85a2006f9ff85d6040e5025ecda8df6c954
Vivek A. Ganesan <onevivek@yahoo.com>
2018-04-06 15:19:26 -0700

    Committing with SDK implemented initially for createAgent and createEntity but without any server API calls

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
+var keypair = require('keypair');
+
+class SignetKeyPair {
+    constructor() {
+        // Create a RSA 2048 Public-Private Key pair
+        Object.defineProperty(this, 'keypair', {
+          value: keypair(),
+          writable: false
+        });
+    }
+
+    // Return the public key of the key pair
+    getPublicKey() {
+        return this.keypair['public'];
+    }
+
+    // Return the private key of the key pair
+    getPrivateKey() {
+        return this.keypair['private'];
+    }
+}
+
+class SignetKeySet {
+    /*
+     * Constructor to create a management key and an encryption key.
+     * The management key will be used to sign for ownership of the
+     * entity and the encryption key will be used to encrypt/decrypt
+     * communication regarding the entity.
+     */
+    constructor() {
+        this.managementKey = new SignetKeyPair();
+        this.encryptionKey = new SignetKeyPair();
+    }
+
+    getPublicKeys() {
+        return [this.managementKey.getPublicKey(), this.encryptionKey.getPublicKey()];
+    }
+}
+
+class SignetEntity {
+    constructor(publicKeys) {
+        // TODO: This should become an API call that returns the ID
+        this.entityID = publicKeys[0];
+    }
+}
+
 class SignetAgent {
     constructor() {
+        // Empty hash containing the entity keys managed by this agent
+        this.keyChain = {};
+    }
+
+    addEntityKeySetToKeyChain(entityID, keySet) {
+        this.keyChain[entityID] = keySet;
+    }
+
+    /*
+     * Method to create an entity which involves the following:
+     *   01) Generate Signet key set
+     *   02) Create an entity on the Signet API
+     *   03) Associate the entity keys with the newly created entity
+     */
+    createEntity() {
+        console.log('Starting createEntity()');
+        var entityKeySet = new SignetKeySet();
+        var entity = new SignetEntity(entityKeySet.getPublicKeys());
+        this.addEntityKeySetToKeyChain(entity.entityID, entityKeySet);
+        console.log('Finished createEntity()');
+        return entity;
     }
 }
 
